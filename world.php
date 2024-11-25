fde6181dd0d361cbfe0c4574437045ee926ec88e
TaraHenry <henrytara1011@gmail.com>
2024-11-25 10:14:09 -0500

    Updated world.php for Exercise 4

--- world.php
+++ world.php
@@ -8,19 +8,43 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 
 if (isset($_GET['country'])) { 
   $country = $_GET['country']; 
-  $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country"); 
+  // Creates a table that includes all countries that match the country entered in the search bar.
+  $stmt = $conn->prepare("SELECT name, continent, independence_year, head_of_state FROM countries WHERE name LIKE :country");
   $stmt->execute(['country' => "%$country%"]); 
 } 
 else 
 { 
-  $stmt = $conn->query("SELECT * FROM countries");
+  // Selects all countries
+  $stmt = $conn->query("SELECT name, continent, independence_year, head_of_state FROM countries");
 }
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<!DOCTYPE html> 
+<html lang="en">
+  <body> 
+    <table> 
+      <thead> 
+        <tr> 
+          <th>Country Name</th> 
+          <th>Continent</th> 
+          <th>Independence Year</th> 
+          <th>Head of State</th> 
+        </tr> 
+      </thead> 
+        
+      <tbody> 
+        <?php foreach ($results as $row): ?> 
+          <tr> 
+            <td><?= $row['name']; ?></td> 
+            <td><?= $row['continent']; ?></td> 
+            <td><?= $row['independence_year']; ?></td>
+            <td><?= $row['head_of_state']; ?></td> 
+          </tr> 
+        <?php endforeach; ?> 
+      </tbody> 
+    </table> 
+  </body> 
+</html>
